3cc7b39b6af78d362a8edc0ef3e79e1da48f6346
Jayesh <jayesh.work@hotmail.com>
2023-09-30 02:15:44 +0530

    API's First Half Completed

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const express = require("express")
+const express = require("express");
+const Validator = require("./helpers/validator");
 
 const PORT = 3000;
-
 const app = express();
-const taskArrData = [];
+// InMemory Resources for Data
+let taskArrData = [];
+let ID_INCREMENT = 0;
+
 
 app.use(express.json())
-// Tasks
+
+// Default
+app.get('/', (req, res) => {   
+    return res.status(200).send("hello world");
+});
+
+// Task Manger API
 // GET /tasks: Retrieve all tasks.
 app.get('/tasks',(req,res)=>{
     return res.status(200).json(taskArrData);
@@ -17,33 +26,56 @@ app.get('/tasks/:taskId',(req,res)=>{
     let taskId = req.params.taskId;
     let task = taskArrData.filter(val=>val.id == taskId);
     if(!task.length){
-        return res.send(404).send("Task not found");
+        return res.status(404).send("Task not found");
     }
-    res.send(200).send(task)
+    res.status(200).send(task)
 })
 
 // POST /tasks : Create a new task.
 app.post('/tasks',(req,res)=>{
     const userProvidedDetails = req.body;
-    console.log(userProvidedDetails);
-    taskArrData.push(userProvidedDetails)
-    return res.status(200).send("Task Created Successfully")
+
+    if(Validator.validateTaskDetails(userProvidedDetails).status){
+        userProvidedDetails['id'] = ++ID_INCREMENT;
+        taskArrData.push(userProvidedDetails);
+        return res.status(201).send(Validator.validateTaskDetails(userProvidedDetails).message)
+    }
+    return res.status(400).json(Validator.validateTaskDetails(userProvidedDetails));
+   
 })
 
 // PUT : Update an existing task by its ID.
 app.put('/tasks/:taskId',(req,res)=>{
     const taskId = req.params.taskId;
     const userProvidedDetails = req.body;
-    console.log(userProvidedDetails);
+    let task = taskArrData.filter(val=>val.id == taskId);
+    
+    if(!task.length){
+        return res.status(404).send("Task not found");
+    }
+
+    if(Validator.validateTaskDetails(userProvidedDetails).status){
+        taskArrData.map((val)=>{
+            if(val.id == taskId){
+                val['title'] = userProvidedDetails.title;
+                val['description'] = userProvidedDetails.description;
+                val['flag'] = userProvidedDetails.flag;
+            }
+        })
+        return res.status(200).send("Task Updated Successfully")
+    }
+    return res.status(400).json(Validator.validateTaskDetails(userProvidedDetails));
 })
 
 
 app.delete('/tasks/:taskId',(req,res)=>{
     const taskId = req.params.taskId;
-    let task = taskArrData.filter(val=>val.id != taskId);
+    let task = taskArrData.filter(val=>val.id == taskId);
     if(!task.length){
-        return res.send(404).send("Task not found");
+        return res.status(404).send("Task not found");
     }
+    taskArrData = taskArrData.filter(val=>val.id != taskId)
+    return res.status(200).send("Task Deleted Successfully")
 })
 
 
